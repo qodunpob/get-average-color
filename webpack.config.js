988a5d9b5f8807a9e6abbd8083232039e8f58145
bashkos <bashkos@gmail.com>
2019-03-18 02:37:38 +0900

    Build docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const { resolve } = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const docsDir = resolve(__dirname, 'docs')
+const docsDistDir = resolve(__dirname, 'docs-dist')
 
-module.exports = {
+const common = {
   context: docsDir,
   entry: './index.js',
 
@@ -15,16 +19,7 @@ module.exports = {
   },
 
   output: {
-    filename: 'bundle.js',
-    path: docsDir,
-    publicPath: '/'
-  },
-
-  devServer: {
-    contentBase: docsDir,
-    publicPath: '/',
-    compress: true,
-    port: 9000
+    filename: 'bundle.js'
   },
 
   resolve: {
@@ -32,4 +27,44 @@ module.exports = {
       'get-average-color': resolve(__dirname, 'src')
     }
   }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = merge(common, {
+    output: {
+      path: docsDistDir
+    },
+
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin(),
+      new CopyPlugin([
+        {
+          from: resolve(docsDir, 'index.html'),
+          to: resolve(docsDistDir, 'index.html')
+        },
+        {
+          from: resolve(docsDir, 'css'),
+          to: resolve(docsDistDir, 'css')
+        },
+        {
+          from: resolve(docsDir, 'img'),
+          to: resolve(docsDistDir, 'img')
+        }
+      ])
+    ]
+  })
+} else {
+  module.exports = merge(common, {
+    output: {
+      path: docsDir,
+      publicPath: '/'
+    },
+
+    devServer: {
+      contentBase: docsDir,
+      publicPath: '/',
+      compress: true,
+      port: 9000
+    }
+  })
 }
